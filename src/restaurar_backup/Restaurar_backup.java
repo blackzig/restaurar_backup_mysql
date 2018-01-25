e8e739f551daeeebda31e93fbc8ee1784b56f320
Michel Adriano Medeiros <michel.medeiros@ti.rc.sp.gov.br>
2018-01-25 13:16:51 -0200

    Restaurando banco de dados MySQL com Java Swing.
    https://www.youtube.com/watch?v=sqF8ylNsyNU

---
+++ src/restaurar_backup/Restaurar_backup.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurar_backup;
+
+/**
+ *
+ * @author michel
+ */
+public class Restaurar_backup {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Restaurar a = new Restaurar();
+        a.setVisible(true);
+    }
+}
